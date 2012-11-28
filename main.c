af2f1b76002767383f5f435bd884321c3cc56bec
Quentin SANTOS <quentin.santos@worldonline.fr>
2012-11-29 00:21:19 +0100

    Minor: removed debug, cleared blank line endings

--- main.c
+++ main.c
@@ -9,7 +9,6 @@
 #include "sha512.h"
 #include "hmac.h"
 #include "cipher.h"
-#include "aes.h" // TODO
 
 void checkDigestStr(void(digest)(uint8_t*, const uint8_t*, uint64_t), char* str, char* hash, uint8_t hashLen)
 {
@@ -83,16 +82,6 @@ int main(int argc, char** argv)
 	return 0;
 */
 
-	// Rijndael test
-	uint8_t I[16] = {0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34};
-	uint8_t K[16] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
-	uint8_t O[16];
-	AES128(K, I, O, false);
-	AES128(K, O, I, true);
-	for (uint8_t i = 0; i < 16; i++)
-		printf("%.2x", I[i]);
-	putchar('\n');
-	return 0;
 	// stdin digest
 /*
 	uint8_t result[20];
