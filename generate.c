b58428b27d5807dcb3bb68098b62b7b5b3df7ebc
Quentin Santos <quentin.santos@worldonline.fr>
2016-09-08 18:17:33 +0200

    Integrate key generation to generator

--- generate.c
+++ generate.c
@@ -3,16 +3,38 @@
 
 #include "md5.h"
 
-int main() {
+const char* charset = "0123456789abcdefghijklmnopqrstuvwxyz";
+size_t charset_length = 36;
+
+size_t get_key(char* dst, size_t length, size_t index) {
+    for (size_t i = length; i --> 0; ) {
+        dst[i] = charset[index % charset_length];
+        index /= charset_length;
+    }
+    return index;
+}
+
+int main(int argc, char** argv) {
     uint32_t count = 0;
     char buffer[1024];
+    size_t index = 0;
     while (1) {
         // get next key
-        fgets(buffer, sizeof(buffer), stdin);
-        if (feof(stdin)) {
-            break;
+        size_t length;
+        if (argc == 1) {
+            fgets((char*) buffer, sizeof(buffer), stdin);
+            if (feof(stdin)) {
+                break;
+            }
+            char* eol = strchr(buffer, '\n');
+            length = (size_t) (eol - buffer);
+        } else {
+            length = 4;
+            if (get_key((char*) buffer, length, index) != 0) {
+                break;
+            }
+            index += 1;
         }
-        size_t length = (size_t) (strchr(buffer, '\n') - buffer);
 
         // hash key
         uint8_t digest[16];
