ed4509d3c6ebb852bc915c8d7e9a2092848dd4d2
Quentin Santos <quentin.santos@worldonline.fr>
2016-09-08 04:54:15 +0200

    Loop unfolding of bytestring comparison

--- sort.c
+++ sort.c
@@ -7,6 +7,13 @@
 
 #define __64BITS__ (UINTPTR_MAX == 0xffffffffffffffff)
 
+// different ordering, friendlier with unfolding (faster)
+#define REVERSE_ENDIAN_ORDERING 1
+// fully reverse the endianness of the ordering (slower)
+#define FULLY_REVERSE_ORDERING 1
+// fix the ordering by reverting all strings (still slower)
+#define REVERT_SORT_REVERT 1
+
 #ifdef _WIN32  //  Windows
 #include <intrin.h>
 uint64_t rdtsc() {
@@ -28,6 +35,68 @@ static void print(uint8_t* addr, size_t size) {
 }
 
 static char bstrncmp(const uint8_t* addr_a, const uint8_t* addr_b, size_t size) {
+#if FULLY_REVERSE_ORDERING
+    addr_a += size;
+    addr_b += size;
+#if __64BITS__
+    while (size >= 8) {
+        size -= 8;
+        addr_a -= 8;
+        addr_b -= 8;
+        uint64_t value_a = *(uint64_t*) addr_a;
+        uint64_t value_b = *(uint64_t*) addr_b;
+        if (value_a != value_b) {
+            return value_a < value_b ? -1 : 1;
+        }
+    }
+#else
+    while (size >= 4) {
+        size -= 4;
+        addr_a -= 4;
+        addr_b -= 4;
+        uint32_t value_a = *(uint32_t*) addr_a;
+        uint32_t value_b = *(uint32_t*) addr_b;
+        if (value_a != value_b) {
+            return value_a < value_b ? -1 : 1;
+        }
+    }
+#endif
+    while (size >= 1) {
+        size -= 1;
+        addr_a -= 1;
+        addr_b -= 1;
+        uint8_t value_a = *addr_a;
+        uint8_t value_b = *addr_b;
+        if (value_a != value_b) {
+            return value_a < value_b ? -1 : 1;
+        }
+    }
+#else
+#if REVERSE_ENDIAN_ORDERING
+#if __64BITS__
+    while (size >= 8) {
+        uint64_t value_a = *(uint64_t*) addr_a;
+        uint64_t value_b = *(uint64_t*) addr_b;
+        if (value_a != value_b) {
+            return value_a < value_b ? -1 : 1;
+        }
+        size -= 8;
+        addr_a += 8;
+        addr_b += 8;
+    }
+#else
+    while (size >= 4) {
+        uint32_t value_a = *(uint32_t*) addr_a;
+        uint32_t value_b = *(uint32_t*) addr_b;
+        if (value_a != value_b) {
+            return value_a < value_b ? -1 : 1;
+        }
+        size -= 4;
+        addr_a += 4;
+        addr_b += 4;
+    }
+#endif
+#endif
     while (size >= 1) {
         uint8_t value_a = *addr_a;
         uint8_t value_b = *addr_b;
@@ -38,6 +107,7 @@ static char bstrncmp(const uint8_t* addr_a, const uint8_t* addr_b, size_t size) 
         addr_a += 1;
         addr_b += 1;
     }
+#endif
     return 0;
 }
 
@@ -70,6 +140,31 @@ static void swap(uint8_t* addr_a, uint8_t* addr_b, size_t size) {
         addr_b += 1;
     }
 }
+
+#if REVERT_SORT_REVERT
+static void reverse(uint8_t* addr, size_t size) {
+    uint8_t* addr_a = addr;
+    uint8_t* addr_b = addr + size;
+#if __64BITS__
+    size /= 2;
+    while (size >= 8) {
+        addr_b -= 8;
+        uint64_t tmp = *(uint64_t*)addr_a;
+        *(uint64_t*)addr_a = __builtin_bswap64(*(uint64_t*)addr_b);
+        *(uint64_t*)addr_b = __builtin_bswap64(tmp);
+        addr_a += 8;
+        size -= 8;
+    }
+#endif
+    while (addr_a < addr_b) {
+        addr_b -= 1;
+        uint8_t tmp = *addr_a;
+        *addr_a = *addr_b;
+        *addr_b = tmp;
+        addr_a += 1;
+    }
+}
+#endif
 
 static void quicksort(uint8_t* start, uint8_t* stop, size_t size) {
     if (start + size >= stop) {
@@ -124,7 +219,19 @@ int main(int argc, char** argv) {
 
     uint64_t timing = rdtsc();
 
+#if REVERT_SORT_REVERT
+    for (uint8_t* i = mem; i < stop; i += size) {
+        reverse(i, size);
+    }
+#endif
+
     quicksort(mem, stop, size);
+
+#if REVERT_SORT_REVERT
+    for (uint8_t* i = mem; i < stop; i += size) {
+        reverse(i, size);
+    }
+#endif
 
     timing = (uint64_t) (rdtsc() - timing);
 
