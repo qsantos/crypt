cd0e7f533e8887fe722c5fb2f818f36fbeadf7a9
Quentin Santos <quentin.santos@worldonline.fr>
2016-09-18 20:34:07 +0200

    Proper argument parsing for `bruteforce`

--- bruteforce.c
+++ bruteforce.c
@@ -3,15 +3,65 @@
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#include "argparse.h"
 #include "util.h"
 #include "keyspace.h"
 #include "md5_simd.h"
 
-int main() {
-    uint8_t target[16];
-    bytes_fromhex(target, "e80b5017098950fc58aad83c8c14978e");  // abcdef
+// parsed arguments
+static struct {
+    uint8_t target[64];
+    int jobs;
+    int single;
+} args = {{0}, 1, 0};
+
+static void argparse(int argc, char** argv) {
+    usage_string = (
+    "Usage: %s [OPTIONS] DIGEST\n"
+    "Try and find the preimage of DIGEST\n"
+    "\n"
+    "DIGEST must be given as an hexadecimal string\n"
+    "\n"
+    "  -1 --single        stop at first preimage found\n"
+    "  -j --jobs N        run on N threads (0 = auto)\n"
+    "  -h --help          display this help and exit\n"
+    "\n"
+    "EXAMPLE\n"
+    "  bruteforce e80b5017098950fc58aad83c8c14978e\n"
+    );
+
+    size_t positional_arguments_read = 0;
+    arginfo.argc = argc;
+    arginfo.argv = argv;
+    for (arginfo.argi = 1; arginfo.argi < argc; arginfo.argi++) {
+        arginfo.arg = argv[arginfo.argi];
+        if (arg_is("--help", "-h")) {
+            usage(NULL);
+        } else if (arg_is("--single", "-1")) {
+            args.single = 1;
+        } else if (arg_is("--jobs", "-j")) {
+            args.jobs = (int) arg_get_int();
+        } else if (arginfo.arg[0] == '-') {
+            usage("unknown option '%s'", arginfo.arg);
+        } else if (positional_arguments_read == 0) {
+            if (bytes_fromhex(args.target, arginfo.arg) < 0) {
+                usage("invalid digest '%s'", arginfo.arg);
+            }
+            positional_arguments_read += 1;
+        } else {
+            usage("too many arguments");
+        }
+    }
+    if (positional_arguments_read < 1) {
+        usage("too few arguments");
+    }
+}
+
+int main(int argc, char** argv) {
+    argparse(argc, argv);
 
     size_t length = 6;
 
@@ -22,6 +72,9 @@ int main() {
 
     size_t stride = 8;
 
+    if (args.jobs >= 1) {
+        omp_set_num_threads(args.jobs);
+    }
     #pragma omp parallel
     {
         size_t thread_id = (size_t) omp_get_thread_num();
@@ -29,7 +82,7 @@ int main() {
         size_t count_per_thread = count / n_threads;
         size_t start = count_per_thread * thread_id;
         fprintf(stderr, "Started thread no %zu / %zu <- keyspace[%zu:%zu]\n",
-               thread_id, n_threads, start, start + count);
+               thread_id+1, n_threads, start, start + count);
 
         uint8_t block[1024] __attribute__((aligned(32)));
         uint8_t interleaved[128];
@@ -41,14 +94,14 @@ int main() {
         size_t n_iterations = (count_per_thread + stride - 1) / stride;
         for (size_t i = 0; i < n_iterations; i += 1) {
             // check whether any of the interleaved candidates match
-            if (md5_test_avx2(target, block)) {
+            if (md5_test_avx2(args.target, block)) {
                 // if so, re-compute the full hash (happens rarely enough)
                 // and redo a full comparison
                 md5_oneblock_avx2(interleaved, block);
                 for (size_t interleaf = 0; interleaf < stride; interleaf += 1) {
                     uint8_t digest[16];
                     uninterleave(digest, interleaved, 16, stride, interleaf);
-                    if (bstrncmp(digest, target, 16) != 0) {
+                    if (bstrncmp(digest, args.target, 16) != 0) {
                         // not an actuall match
                         continue;
                     }
@@ -59,6 +112,10 @@ int main() {
                     get_key(buffer, length, index);
                     buffer[length] = '\0';
                     printf("%s\n", buffer);
+
+                    if (args.single) {
+                        exit(0);
+                    }
                 }
             }
 
