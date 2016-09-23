bb30ad371c6bbdd83388d668c2591ff0c66967ca
Quentin Santos <quentin.santos@worldonline.fr>
2016-09-23 18:52:55 +0200

    Fix cheat in architecture targetting

--- mmx.c
+++ mmx.c
@@ -1,6 +1,5 @@
 #include <stdint.h>
 #include <mmintrin.h>  // MMX
-#include <emmintrin.h>  // SSE2 (for _mm_movemask_epi8)
 
 // rotate packed 32-bit integers
 __attribute__((target("mmx")))
@@ -19,6 +18,13 @@ static inline __m64 my_mm_bswap_pi32(__m64 x) {
     return x;
 }
 
+// test equality of packed 32-bit integers with an immediate
+__attribute__((target("mmx")))
+static inline int my_mm_anyeq_pi32(__m64 x, uint32_t imm) {
+    uint64_t y = (uint64_t) _m_to_int64(x);
+    return ((y & 0xffffffff) == imm) || ((y >> 32) == imm);
+}
+
 #define WORD __m64
 #define OR(a, b) _mm_or_si64(a, b)
 #define XOR(a, b) _mm_xor_si64(a, b)
@@ -26,8 +32,7 @@ static inline __m64 my_mm_bswap_pi32(__m64 x) {
 #define ANDNOT(a, b) _mm_andnot_si64(a, b)
 #define ROL(x,n) my_mm_rol_pi32(x, n)
 #define ADD(a, b) (_mm_add_pi32((a), (b)))
-// TODO: cheating with _mm_movemask_epi8 (SSE instruction)
-#define ANY_EQ(X, V) _mm_movemask_pi8(_mm_cmpeq_pi32(X, SET1(V)));
+#define ANY_EQ(X, V) my_mm_anyeq_pi32(X, V)
 #define BSWAP(X) my_mm_bswap_pi32(X)
 #define SET1(a) (_mm_set1_pi32((int) (a)))
 
