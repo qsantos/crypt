a12dd5e7bb86491d206e7e42d27b70c2d157eb66
Quentin Santos <quentin.santos@worldonline.fr>
2016-09-28 04:54:46 +0200

    Inverse iteration order

--- keyspace.h
+++ keyspace.h
@@ -13,9 +13,10 @@ static size_t charset_length = sizeof(charset) - 1;
 
 static inline int key_index(const char* key, size_t* ret) {
     size_t index = 0;
-    for (const char* c = key; *c; c += 1) {
+    size_t length = strlen(key);
+    for (size_t i = length; i --> 0; ) {
         index *= charset_length;
-        char* pos = strchr(charset, *c);
+        char* pos = strchr(charset, key[i]);
         if (pos == NULL) {
             return -1;
         }
@@ -26,7 +27,7 @@ static inline int key_index(const char* key, size_t* ret) {
 }
 
 static inline size_t get_key(char* dst, size_t length, size_t index) {
-    for (size_t i = length; i --> 0; ) {
+    for (size_t i = 0; i < length; i += 1) {
         dst[i] = charset[index % charset_length];
         index /= charset_length;
     }
@@ -43,7 +44,7 @@ static inline void set_key_im(uint8_t* block, const char* key, size_t stride) {
 static inline void set_keys(uint8_t* block, const char** ptrs, size_t length, size_t stride, size_t index, size_t index_stride) {
     for (size_t interleaf = 0; interleaf < stride; interleaf += 1) {
         size_t current_index = index + interleaf * index_stride;
-        for (size_t i = length; i --> 0; ) {
+        for (size_t i = 0; i < length; i += 1) {
             size_t offset = interleaved_offset(i, stride, interleaf);
             ptrs[offset] = &charset[current_index % charset_length];
             block[offset] = (uint8_t) *ptrs[offset];
@@ -55,7 +56,7 @@ static inline void set_keys(uint8_t* block, const char** ptrs, size_t length, si
 
 static inline void next_keys(uint8_t* block, const char** ptrs, size_t length, size_t stride) {
     for (size_t interleaf = 0; interleaf < stride; interleaf += 1) {
-        for (size_t i = length; i --> 0; ) {
+        for (size_t i = 0; i < length; i += 1) {
             size_t offset = interleaved_offset(i, stride, interleaf);
             ptrs[offset] += 1;
             // likely() does make a significant difference
